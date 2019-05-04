36dd6185e256b6c8f7ae397ade1a026de05b3b68
joncrall <erotemic@gmail.com>
2019-05-03 23:04:16 -0400

    wip

--- init_docs.sh
+++ init_docs.sh
@@ -107,7 +107,7 @@ init_docs(){
 
     # Build documentation in the docs/ directory with Sphinx
     sphinx:
-      configuration: docs/souce/conf.py
+      configuration: docs/source/conf.py
 
     # Optionally build your docs in additional formats such as PDF and ePub
     formats: all
