903a1587642f21ebdf90ccd9ccafb8e517ae0fa1
joncrall <erotemic@gmail.com>
2021-01-28 13:17:36 -0500

    wip

--- bump_versions.sh
+++ bump_versions.sh
@@ -430,9 +430,11 @@ mypkgs(){
     finish_deployment $MODNAME $DEPLOY_REMOTE $DEPLOY_BRANCH
 
     source ~/misc/bump_versions.sh
+    load_secrets
     MODNAME=ndsampler
     DEPLOY_REMOTE=public
     DEPLOY_BRANCH=release
+    accept_latest_gitlab_dev_mr $MODNAME $DEPLOY_REMOTE
     update_master $MODNAME $DEPLOY_REMOTE
     finish_deployment $MODNAME $DEPLOY_REMOTE $DEPLOY_BRANCH
 
