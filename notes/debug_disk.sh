e568e2ef02eaed7a3f6db4fa2c77931a185db283
joncrall <jon.crall@kitware.com>
2021-07-11 15:39:54 -0400

    wip

---
+++ notes/debug_disk.sh
@@ -0,0 +1,8 @@
+sudo smartctl --all /dev/sda 
+sudo smartctl --all /dev/sdb
+sudo smartctl --all /dev/sdc 
+sudo smartctl --all /dev/sdd
+
+sudo smartctl --all /dev/nvme0n1
+
+lsblk
--- notes/hardwareinfo/memory_debug.py
+++ notes/hardwareinfo/memory_debug.py
@@ -20,8 +20,6 @@ def checker():
 
         ((0x700000000 * reg.byte), (0xE00000000 * reg.byte)),
 
-        ((0xE30000000 * reg.byte), (0x1060000000 * reg.byte)),
-
         ((0xE24000000 * reg.byte), (0x1060000000 * reg.byte)),
         # ((0xF00000000 * reg.byte), (0x1060000000 * reg.byte)),
     ]
@@ -68,3 +66,224 @@ def checker():
         print('-----')
 
     # (0x600000000 * reg.byte).to('gigabyte')
+
+    # https://unix.stackexchange.com/questions/75059/how-to-blacklist-a-correct-bad-ram-sector-according-to-memtest86-error-indicati
+    # https://askubuntu.com/questions/908925/how-do-i-tell-ubuntu-not-to-use-certain-memory-addresses
+
+    # https://stackoverflow.com/questions/8213671/mmap-operation-not-permitted
+
+    # Example
+
+    # The second parameter is a mask.
+    # You put 1s where the address range you want shares the same values
+    # You put 0s where it will vary.
+
+    import numpy as np
+    start = np.uint64(0x7DDF0000)
+    stop  = np.uint64(0x7DDF4000) - np.uint64(1)
+    target = np.uint64(0xffffc000)
+
+    import numpy as np
+    nbytes = 64
+    dtype = np.uint64
+    # Starting bad mem address range
+    start = dtype(0x7DDF0000)
+    # Subtract 1 because to make the stop range inclusive instead of exclusive
+    stop  = dtype(0x7DDF4000) - dtype(1)
+
+    def xnor(a, b):
+        return ~(a ^ b)
+
+    def find_pos_of_leftmost_sig_zero(a, nbytes=64):
+        """
+        Given a bit string like, we find the position of the first zero bit we
+        see coming from the left, such that we can right-bitshift to it to the
+
+            11111110110110
+                   ^
+
+        Example:
+            >>> nbytes = 32
+            >>> vals = [0b0, 0b1, 0b1011, 0b1001]
+            >>> for a in vals:
+            >>>     print('-----')
+            >>>     pos = find_pos_of_leftmost_sig_zero(a, nbytes)
+            >>>     print(f'a = {a:032b}')
+            >>>     print('    ' + ' ' * (nbytes - pos - 1) + '^')
+            >>>     print(f'pos = {pos}')
+            -----
+            a = 00000000000000000000000000000000
+                                               ^
+            pos = 0
+            -----
+            a = 00000000000000000000000000000001
+                                               ^
+            pos = 0
+            -----
+            a = 00000000000000000000000000001011
+                                             ^
+            pos = 2
+            -----
+            a = 00000000000000000000000000001001
+                                             ^
+            pos = 2
+        """
+        # not really a fast op, but it works well enough There is a semi-corner
+        # case at 1 and 0, but it doesnt change how we use it
+        binstr = ('{:0' + str(nbytes) + 'b}').format(a)
+        try:
+            leftmost_one = binstr.index('1')
+        except ValueError:
+            return 0
+        try:
+            leftmost_sig_zero = binstr[leftmost_one:].index('0') + leftmost_one + 1
+        except Exception:
+            return 0
+        # Flip string indexes to bit indexes
+        return nbytes - leftmost_sig_zero
+
+    # Find all the bits in common
+    common = xnor(start, stop)
+
+    # Find the position of the first zero (non-common bit) we see from the left
+    shift = dtype(find_pos_of_leftmost_sig_zero(common, nbytes))
+
+    # Right then left shift to zero out the tail, which constructs the mask
+    mask = (common >> shift) << (shift)
+
+    print(f'start  = 0b{start:64b} = 0x{start:16x}')
+    print(f'stop   = 0b{stop:64b} = 0x{stop:16x}')
+    print(f'common = 0b{common:64b} = 0x{common:16x}')
+    print(f'mask   = 0b{mask:64b} = 0x{mask:16x}')
+
+    print('--')
+    print(f'mask = ' + hex(mask))
+
+    print(f'0b{target:64b}')
+
+    # start = 0x7DDF0000
+    0b01111101110111110000000000000000
+    # stop = 0x7DDF4000
+    0b01111101110111110100000000000000
+
+    0b0111110111011111 0100 0000 0000 0000
+    0b1111101110111110100000000000000
+    # Common =
+    0b11111111111111111011111111111111
+
+    # 0xffff8000
+    0b11111111111111111000000000000000
+    # 0xffffc000
+    0b11111111111111111100000000000000
+
+    0b1111111111111111111111111111111111111111111111111011111111111111
+                     b100000000000000
+    0b1111111111111111000000000000000
+    hex(0b1111111111111111100000000000000)
+
+
+    # Real
+
+    ((0x700000000 - 0x600000000) * reg.byte).to('megabyte')
+    ((0xE24000000 - 0xE00000000) * reg.byte).to('megabyte')
+
+    find_grub_badram_format(0x700000000, 0x600000000)
+
+    0b11000000000000000000000000000000000
+    0b11100000000000000000000000000000000
+
+    start = 0x600000000
+    stop = 0x700000000
+    mask = start & stop
+    start & mask
+
+    print('0X{:09X}'.format(start ^ stop))
+
+    '''
+    # Using the memtester trick does not work if the kernel does not
+    # allow users to address physical RAM space.
+
+    sudo memtester -p 0x600000000 4295M 128
+    sudo memtester -p 0xE00000000 604M 128
+    '''
+
+
+def find_grub_badram_format(start, stop):
+    import numpy as np
+    nbytes = 64
+    dtype = np.uint64
+    # Starting bad mem address range
+    start = dtype(start)
+    # Subtract 1 because to make the stop range inclusive instead of exclusive
+    stop  = dtype(stop) - dtype(1)
+
+    def xnor(a, b):
+        return ~(a ^ b)
+
+    def find_pos_of_leftmost_sig_zero(a, nbytes=64):
+        """
+        Given a bit string like, we find the position of the first zero bit we
+        see coming from the left, such that we can right-bitshift to it to the
+
+            11111110110110
+                   ^
+
+        Example:
+            >>> nbytes = 32
+            >>> vals = [0b0, 0b1, 0b1011, 0b1001]
+            >>> for a in vals:
+            >>>     print('-----')
+            >>>     pos = find_pos_of_leftmost_sig_zero(a, nbytes)
+            >>>     print(f'a = {a:032b}')
+            >>>     print('    ' + ' ' * (nbytes - pos - 1) + '^')
+            >>>     print(f'pos = {pos}')
+            -----
+            a = 00000000000000000000000000000000
+                                               ^
+            pos = 0
+            -----
+            a = 00000000000000000000000000000001
+                                               ^
+            pos = 0
+            -----
+            a = 00000000000000000000000000001011
+                                             ^
+            pos = 2
+            -----
+            a = 00000000000000000000000000001001
+                                             ^
+            pos = 2
+        """
+        # not really a fast op, but it works well enough There is a semi-corner
+        # case at 1 and 0, but it doesnt change how we use it
+        binstr = ('{:0' + str(nbytes) + 'b}').format(a)
+        try:
+            leftmost_one = binstr.index('1')
+        except ValueError:
+            return 0
+        try:
+            leftmost_sig_zero = binstr[leftmost_one:].index('0') + leftmost_one + 1
+        except Exception:
+            return 0
+        # Flip string indexes to bit indexes
+        return nbytes - leftmost_sig_zero
+
+    # Find all the bits in common
+    common = xnor(start, stop)
+
+    # Find the position of the first zero (non-common bit) we see from the left
+    shift = dtype(find_pos_of_leftmost_sig_zero(common, nbytes))
+
+    # Right then left shift to zero out the tail, which constructs the mask
+    mask = (common >> shift) << (shift)
+
+    print(f'start  = 0b{start:64b} = 0x{start:16x}')
+    print(f'stop   = 0b{stop:64b} = 0x{stop:16x}')
+    print(f'common = 0b{common:64b} = 0x{common:16x}')
+    print(f'mask   = 0b{mask:64b} = 0x{mask:16x}')
+
+    print('--')
+    print(f'mask = ' + hex(mask))
+
+    badram_format = f'0x{start:0{16}x},0x{mask:#0{16}x}'
+    print(badram_format)
