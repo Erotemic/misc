5be716e1b0295d07dace6b06207cc38c229d6e36
joncrall <erotemic@gmail.com>
2021-07-27 11:47:44 -0400

    wip

--- learn/learn_gdal_grid.py
+++ learn/learn_gdal_grid.py
@@ -1,40 +1,46 @@
-from os.path import exists
-import ubelt as ub
-import geopandas as gpd
-import kwarray
-import kwimage
 
 
 def main():
-    # https://gis.stackexchange.com/questions/254330/python-gdal-grid-correct-use
-    # https://gdal.org/programs/gdal_grid.html
+    """
+    Demo using gdal_grid
 
-    world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
+    References:
+        https://gis.stackexchange.com/questions/254330/python-gdal-grid-correct-use
+        https://gdal.org/programs/gdal_grid.html
 
+    """
+    from os.path import exists
+    import ubelt as ub
+    import geopandas as gpd
+    import numpy as np
+
+    # Generate Demo Data Ungridded data
+    # Generate a random value at points along the African continent
     xyz = []
-
-    rng = kwarray.ensure_rng(432432)
-
+    world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
+    rng = np.random.RandomState(0)
     for shape in world[world['continent'] == 'Africa'].geometry:
-        mpoly = kwimage.MultiPolygon.from_shapely(shape)
-        for poly in mpoly.data:
-            exterior = poly.data['exterior']
-
-            for x, y in exterior.data:
+        if shape.type == 'MultiPolygon':
+            for geom in shape.geoms:
+                for x, y in geom.exterior.coords:
+                    z = rng.rand()
+                    xyz.append((x, y, z))
+        elif shape.type == 'Polygon':
+            for x, y in shape.exterior.coords:
                 z = rng.rand()
                 xyz.append((x, y, z))
-
-    import numpy as np
     xyz = np.array(xyz)
 
+    # Write the demo data to a CSV file
     lines = ['x;y;d']
     for x, y, z in xyz:
         lines.append(f'{x};{y};{z}')
     text = '\n'.join(lines)
-
     with open('tmp.csv', 'w') as file:
         file.write(text)
 
+    # I was unable to get the command to work with the CSV file alone
+    # I needed to create a VRT that pointed at the CVS file.
     with open('tmp.vrt', 'w') as file:
         file.write(ub.codeblock(
             '''
@@ -49,16 +55,16 @@ def main():
             </OGRVRTDataSource>
             '''))
 
+    # Can modify these but they are not needed
     minx, miny = xyz.T[0:2].min(axis=1)
     maxx, maxy = xyz.T[0:2].max(axis=1)
-
     lonext = maxx - minx
     latext = maxy - miny
-
     ar = latext / lonext
     xres = int(512)
     yres = int(xres * ar)
 
+    # Call the GDAL Grid command
     command = (
         'gdal_grid '
         # '-ot Float32 -of GTiff '
@@ -75,15 +81,15 @@ def main():
     ub.delete('tmp.tif')
     print(command)
     info = ub.cmd(command, verbose=3)
-
     assert info['ret'] == 0
 
     if exists('tmp.tif'):
-        data = kwimage.imread('tmp.tif')
+        # Show the result if it worked
+        import tifffile
+        data = tifffile.imread('tmp.tif')
+        from matplotlib import pyplot as plt
         print('data.shape = {!r}'.format(data.shape))
-        import kwplot
-        kwplot.autompl()
-        kwplot.imshow(data)
+        plt.imshow(data)
 
 
 if __name__ == '__main__':
