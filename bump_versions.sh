05481097ce1c3f636561db364462dc40dfb4ecd6
joncrall <jon.crall@kitware.com>
2022-07-18 11:36:53 -0400

    wip

--- bump_versions.sh
+++ bump_versions.sh
@@ -519,6 +519,16 @@ mypkgs(){
     finish_deployment $MODNAME $DEPLOY_REMOTE $DEPLOY_BRANCH
     create_new_gitlab_dev_mr $MODNAME $DEPLOY_REMOTE 
 
+    source ~/misc/bump_versions.sh
+    load_secrets
+    MODNAME=cmd_queue
+    DEPLOY_REMOTE=origin
+    DEPLOY_BRANCH=release
+    accept_latest_gitlab_dev_mr $MODNAME $DEPLOY_REMOTE
+    update_default_branch $MODNAME $DEPLOY_REMOTE
+    finish_deployment $MODNAME $DEPLOY_REMOTE $DEPLOY_BRANCH
+    create_new_gitlab_dev_mr $MODNAME $DEPLOY_REMOTE 
+
 
     ### GITHUB PROJECTS
 
