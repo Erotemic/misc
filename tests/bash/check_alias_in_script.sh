5a99f0ce37888d9a943073f2463cb1c67727f0af
joncrall <jon.crall@kitware.com>
2022-08-24 01:18:08 -0400

    wip

--- tests/bash/check_alias_in_script.sh
+++ tests/bash/check_alias_in_script.sh
@@ -17,6 +17,9 @@ DEF_FPATH=$TEMP_DPATH/definitions.sh
 echo '
 #!/bin/bash
 echo "[definitions] start"
+export MYDEF=cowsay
+# DONT USE ALIASES THEY DO NOT EXPAND UNLESS expand_aliases in shopt is set!
+# https://www.gnu.org/software/bash/manual/html_node/Aliases.html
 alias mydef=cowsay
 echo "[definitions] finish"
 ' > "$DEF_FPATH"
@@ -27,9 +30,13 @@ USAGE_FPATH=$TEMP_DPATH/usage.sh
 echo '
 #!/bin/bash
 echo "[usage] about to source definitions"
+#shopt -s expand_aliases
 source "'"$TEMP_DPATH"/definitions.sh'"
 echo "[usage] about to use the definition"
+#shopt
+#shopt
 mydef "hello world"
+#$MYDEF "hello world"
 echo "[usage] finished"
 ' > "$USAGE_FPATH"
 
@@ -59,7 +66,9 @@ echo ""
 echo ""
 echo "Variant #1: Execute with source (this works)"
 echo "-------------------"
+set +x
 source "$USAGE_FPATH"
+set +x
 echo "-------------------"
 
 
@@ -67,5 +76,27 @@ echo ""
 echo ""
 echo "Variant #2: Execute with bash -c (this fails)"
 echo "--------------------"
-bash -c "source '$USAGE_FPATH'"
+set +x
+/bin/bash -c "source '$USAGE_FPATH'"
+set +x
+echo "--------------------"
+
+
+echo ""
+echo ""
+echo "Variant #3: Execute with bash -ci (this works)"
+echo "--------------------"
+set +x
+/bin/bash -ci "source '$USAGE_FPATH'"
+set +x
+echo "--------------------"
+
+
+echo ""
+echo ""
+echo "Variant #4: Execute with bash -c with shopt -s expand_aliases (this works)"
+echo "--------------------"
+set +x
+/bin/bash -ci "shopt -s expand_aliases && source '$USAGE_FPATH'"
+set +x
 echo "--------------------"
