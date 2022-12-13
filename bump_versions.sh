fc02f5c9ad5615da1d935c4df4245c0742a85214
joncrall <erotemic@gmail.com>
2022-12-13 11:09:34 -0500

    wip

--- bump_versions.sh
+++ bump_versions.sh
@@ -433,11 +433,15 @@ mypkgs(){
     finish_deployment $MODNAME $DEPLOY_REMOTE $DEPLOY_BRANCH
     create_new_gitlab_dev_mr $MODNAME $DEPLOY_REMOTE 
 
+    source ~/misc/bump_versions.sh
+    load_secrets
     MODNAME=liberator
     DEPLOY_REMOTE=origin
     DEPLOY_BRANCH=release
+    accept_latest_gitlab_dev_mr $MODNAME $DEPLOY_REMOTE
     update_default_branch $MODNAME $DEPLOY_REMOTE
     finish_deployment $MODNAME $DEPLOY_REMOTE $DEPLOY_BRANCH
+    create_new_gitlab_dev_mr $MODNAME $DEPLOY_REMOTE 
 
     source ~/misc/bump_versions.sh
     load_secrets
