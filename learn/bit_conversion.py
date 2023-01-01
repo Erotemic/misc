fde948db05f05b5e20caddaa3a7a42498f275b11
joncrall <erotemic@gmail.com>
2023-01-01 12:33:12 -0500

    wip

--- learn/bit_conversion.py
+++ learn/bit_conversion.py
@@ -110,6 +110,69 @@ def bit_positions_lut(val):
     return bit_positions
 
 
+def unpack_bit_positions(val, itemsize=None):
+    """
+    Given an integer value, return the positions of the on bits.
+
+    Args:
+        val (int): a signed or unsigned integer
+
+        itemsize (int | None):
+            Number of bytes used to represent the integer. E.g. 1 for a uint8 4
+            for an int32. If unspecified infer the smallest number of bytes
+            needed, but warning this may produce ambiguous results for negative
+            numbers.
+
+    Returns:
+        List[int]: the indexes of the 1 bits.
+
+    Note:
+        This turns out to be faster than a numpy or lookuptable strategy I
+        tried.  See github.com:Erotemic/misc/learn/bit_conversion.py
+
+    Example:
+        >>> unpack_bit_positions(0)
+        []
+        >>> unpack_bit_positions(1)
+        [0]
+        >>> unpack_bit_positions(-1)
+        [0, 1, 2, 3, 4, 5, 6, 7]
+        >>> unpack_bit_positions(-1, itemsize=2)
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+        >>> unpack_bit_positions(9)
+        [0, 3]
+        >>> unpack_bit_positions(2132)
+        [2, 4, 6, 11]
+        >>> unpack_bit_positions(-9999)
+        [0, 4, 5, 6, 7, 11, 12, 14, 15]
+        >>> unpack_bit_positions(np.int16(-9999))
+        [0, 4, 5, 6, 7, 11, 12, 14, 15]
+        >>> unpack_bit_positions(np.int16(-1))
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+    """
+    is_negative = val < 0
+    if is_negative:
+        if itemsize is None:
+            try:
+                bit_length = val.bit_length() + 1
+                itemsize = math.ceil(bit_length / 8.0)  # bytelength
+            except AttributeError:
+                # Probably a numpy type
+                itemsize = val.dtype.itemsize
+        neg_position = (itemsize * 8) - 1
+        # special logic for negatives to get twos compliment repr
+        max_val = 1 << neg_position
+        val_ = max_val + val
+    else:
+        val_ = val
+    binary_string = '{:b}'.format(val_)[::-1]
+    bit_positions = [pos for pos, char in enumerate(binary_string)
+                     if char == '1']
+    if is_negative:
+        bit_positions.append(neg_position)
+    return bit_positions
+
+
 if __name__ == '__main__':
     """
     CommandLine:
