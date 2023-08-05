86f8a7dc5e6df927331178b92bcf0efada8b4916
joncrall <erotemic@gmail.com>
2023-08-05 17:28:38 -0400

    wip

--- tests/python/dict_set_discussion.py
+++ tests/python/dict_set_discussion.py
@@ -4,30 +4,54 @@ from typing import Iterable
 import random
 
 
-def __and__(self: dict, other: Iterable):
+class CandidateOperations:
     """
-    Proposed new "intersection-like" method for dictionaries
-
-    Example:
-        >>> self = {'a': 1, 'b': 2}
-        >>> other = {'b': 1, 'c': 3}
-        >>> __and__(self, other)
+    Container to organize candidate implementations we will test
     """
-    common = self.keys() & other
-    return self.__class__((k, self[k]) for k in common)
 
+    def __and_v1__(self: dict, other: Iterable):
+        """
+        Intersection-like method for dictionaries based on existing key
+        intersection
 
-def __sub__(self: dict, other: Iterable):
-    """
-    Proposed new "difference-like" method for dictionaries
+        Example:
+            >>> self = {'a': 1, 'b': 2}
+            >>> other = {'b': 1, 'c': 3}
+            >>> __and_v1__(self, other)
+        """
+        common = self.keys() & other
+        return self.__class__((k, self[k]) for k in common)
 
-    Example:
-        >>> self = {'a': 1, 'b': 2}
-        >>> other = {'b': 1, 'c': 3}
-        >>> __sub__(self, other)
-    """
-    common = self.keys() - other
-    return self.__class__((k, self[k]) for k in common)
+    def __and_v2__(self, other):
+        """
+        Alternative __sub__ that preserves LHS order
+        """
+        isect_keys = set(self.keys())
+        isect_keys.intersection_update(other)
+        new = self.__class__((k, self[k]) for k in self if k in isect_keys)
+        return new
+
+    def __sub_v1__(self: dict, other: Iterable):
+        """
+        Difference-like method for dictionaries based on existing key
+        difference
+
+        Example:
+            >>> self = {'a': 1, 'b': 2}
+            >>> other = {'b': 1, 'c': 3}
+            >>> __sub_v1__(self, other)
+        """
+        common = self.keys() - other
+        return self.__class__((k, self[k]) for k in common)
+
+    def __sub_v2__(self, other):
+        """
+        Alternative __sub__ that preserves LHS order
+        """
+        diff_keys = set(self.keys())
+        diff_keys.difference_update(other)
+        new = self.__class__((k, self[k]) for k in self if k in diff_keys)
+        return new
 
 
 class mydefaultdict(defaultdict):
@@ -70,7 +94,7 @@ class TestCaseGenerator:
     """
     Generates random data for RHS and LHS of proposed dictionary operation
     """
-    def __init__(testgen, max_size, rng):
+    def __init__(self, max_size, rng, niter):
         # Get a list of items that are equal but non-identical
         key_alternatives = []
         for k in range(0, max_size):
@@ -78,42 +102,119 @@ class TestCaseGenerator:
             if k in {0, 1}:
                 alts.append(bool(k))
             key_alternatives.append(alts)
-        testgen.key_alternatives = key_alternatives
-        testgen.rng = rng
+        self.key_alternatives = key_alternatives
+        self.rng = rng
+        self.niter = niter
 
-    def _random_keys(testgen):
-        rng = testgen.rng
+    def _random_keys(self):
+        rng = self.rng
         # Randomize the number of keys in the RHS and LHS
-        size = rng.randint(0, len(testgen.key_alternatives))
+        size = rng.randint(0, len(self.key_alternatives))
         # Randomize the values of the keys in the RHS and LHS
-        alts = choice_without_replacement(rng, testgen.key_alternatives, k=size)
+        alts = choice_without_replacement(rng, self.key_alternatives, k=size)
         keys = [rng.choice(ks) for ks in alts]
         # Randomize the order of the keys in the RHS and LHS
         rng.shuffle(keys)
         return keys
 
-    def random_lhs(testgen):
-        keys1 = testgen._random_keys()
+    def random_lhs(self):
+        keys1 = self._random_keys()
         # Make a random LHS dictionary
         lhs = {k: idx for idx, k in enumerate(keys1)}
         return lhs
 
-    def random_rhs(testgen):
-        keys2 = testgen._random_keys()
+    def random_rhs(self):
+        keys2 = self._random_keys()
         # The RHS can be anything iterable, here are some common choices
-        rhs_types = [set, list, dict, tuple, mydefaultdict]
+        # rhs_types = [set, list, dict, tuple, mydefaultdict]
+        rhs_types = [set, list, dict, tuple]
 
         # Make a random RHS iterable
         # Choose some iterable type for the "other" argument
-        rhs_type = testgen.rng.choice(rhs_types)
+        rhs_type = self.rng.choice(rhs_types)
         if issubclass(rhs_type, Mapping):
             rhs = rhs_type((k, idx) for idx, k in enumerate(keys2))
         else:
             rhs = rhs_type(keys2)
         return rhs
 
+    def next_case(self):
+        lhs = self.random_lhs()
+        rhs = self.random_rhs()
+        return {'lhs': lhs, 'rhs': rhs}
+
+    def __iter__(self):
+        for _ in range(self.niter):
+            yield  self.next_case()
+
+
+class ExemplarCaseGenerator:
+
+    def __init__(self):
+        self.exemplars = [
+            {'lhs': {3: 0, 0: 1, 2.0: 2}, 'rhs': [3.0, 2, 1.0, 4.0, 0]},
+            {'lhs': {4.0: 0, True: 1, 2.0: 2, 0.0: 3}, 'rhs': {4.0: 0, True: 1, 3: 2, 0.0: 3}},
+            {'lhs': {False: 0, 3: 1, 1: 2}, 'rhs': {3: 0, True: 1, 4: 2, False: 3, 2: 4}},
+            {'lhs': {4: 0}, 'rhs': [0]},
+            {'lhs': {2: 0, 3: 1, 1: 2, 4.0: 3}, 'rhs': {0.0, 1, 2, 3, 4}},
+            {'lhs': {}, 'rhs': {}},
+            {'lhs': {4: 0, False: 1, 2: 2, 3.0: 3, 1: 4}, 'rhs': {1: 0, 4: 1, 0: 2, 3.0: 3, 2.0: 4}},
+            {'lhs': {0.0: 0, 3.0: 1, 2.0: 2, 4.0: 3}, 'rhs': {0.0, 1.0, 2.0, 4}},
+            {'lhs': {4.0: 0}, 'rhs': {}},
+            {'lhs': {1.0: 0, 2.0: 1, 0: 2}, 'rhs': (0, True, 2, 3.0)},
+            {'lhs': {2: 0, 4: 1, True: 2, False: 3}, 'rhs': {4.0: 0, 1: 1, 0: 2}},
+            {'lhs': {1.0: 0, 4: 1, False: 2, 3: 3, 2: 4}, 'rhs': [3, True, 0.0]},
+            {'lhs': {4: 0, False: 1, 2: 2, True: 3, 3.0: 4}, 'rhs': {1.0: 0, 4: 1, 0.0: 2, 3.0: 3, 2.0: 4}},
+            {'lhs': {2.0: 0, True: 1, 0: 2, 4.0: 3, 3: 4}, 'rhs': {2, 3, 4.0}},
+            {'lhs': {False: 0, 1: 1, 3.0: 2}, 'rhs': {3.0: 0, 0.0: 1, 1: 2, 4: 3}},
+            {'lhs': {True: 0, 0.0: 1, 4: 2, 3: 3, 2: 4}, 'rhs': {0.0, True, 4}},
+            {'lhs': {False: 0, 2: 1, 3.0: 2, 1.0: 3}, 'rhs': {0.0: 0, 4: 1, 3.0: 2}},
+            {'lhs': {2: 0, 4.0: 1, True: 2}, 'rhs': (4.0, 2, False, 3.0)},
+            {'lhs': {}, 'rhs': {4}},
+            {'lhs': {1.0: 0, 3: 1, 4.0: 2, 2.0: 3}, 'rhs': {False, True, 2.0, 3, 4}},
+            {'lhs': {1.0: 0, 3: 1, 0.0: 2, 2: 3}, 'rhs': {False, 1.0, 2, 3}},
+            {'lhs': {4.0: 0}, 'rhs': []},
+            {'lhs': {}, 'rhs': {1.0: 0}},
+            {'lhs': {3.0: 0, 2.0: 1, 1: 2, 4: 3}, 'rhs': (False, 1.0, 4.0, 3.0, 2.0)},
+            {'lhs': {3.0: 0, 2: 1, 0.0: 2, 1.0: 3, 4: 4}, 'rhs': (0, 3, 1, 4.0, 2.0)},
+            {'lhs': {4: 0, 0: 1, True: 2, 2.0: 3}, 'rhs': (0.0, 3, 2)},
+            {'lhs': {False: 0, 3.0: 1, True: 2}, 'rhs': {False: 0, 2.0: 1, 3: 2, 4.0: 3}},
+            {'lhs': {2.0: 0, 0: 1, 3.0: 2, 4.0: 3}, 'rhs': {False, 1.0, 3}},
+            {'lhs': {4: 0, 2.0: 1, False: 2}, 'rhs': {0, True, 3.0, 4}},
+            {'lhs': {1.0: 0}, 'rhs': [2.0]},
+            {'lhs': {3: 0, 4.0: 1, True: 2}, 'rhs': {1: 0, 2.0: 1, 3.0: 2}},
+            {'lhs': {2.0: 0, True: 1, 3: 2}, 'rhs': {True: 0, 2: 1, False: 2, 3: 3, 4.0: 4}},
+            {'lhs': {2.0: 0, True: 1, 4.0: 2, 3: 3}, 'rhs': {2.0, 3, 4}},
+            {'lhs': {3: 0, 2.0: 1, 1: 2, 4: 3}, 'rhs': {4: 0, 2: 1, 1: 2}},
+            {'lhs': {}, 'rhs': []},
+            {'lhs': {0.0: 0, 3: 1, 4.0: 2, 2: 3, True: 4}, 'rhs': (3, 2, 4, False)},
+            {'lhs': {3.0: 0, 0.0: 1, 4.0: 2}, 'rhs': {3.0: 0, 4: 1, 1.0: 2}},
+            {'lhs': {False: 0, 2: 1, 4: 2, 3: 3}, 'rhs': (2, 4.0, 3, True, False)},
+            {'lhs': {4.0: 0}, 'rhs': (2,)},
+            {'lhs': {3.0: 0, 2: 1, False: 2, 4.0: 3}, 'rhs': {1: 0, 2: 1, 3: 2, 4.0: 3, 0: 4}},
+            {'lhs': {True: 0}, 'rhs': {}},
+            {'lhs': {}, 'rhs': {2: 0}},
+            {'lhs': {2: 0, 3.0: 1, True: 2, 0: 3}, 'rhs': {0.0, 2, 4}},
+            {'lhs': {}, 'rhs': {}},
+            {'lhs': {4.0: 0, 3: 1, False: 2, 2.0: 3}, 'rhs': {2: 0, 3.0: 1, True: 2}},
+            {'lhs': {}, 'rhs': {1: 0}},
+            {'lhs': {3: 0, 2.0: 1, 0: 2, 1.0: 3}, 'rhs': {0.0, 1.0, 2.0, 4.0}},
+            {'lhs': {False: 0, 2.0: 1, 4.0: 2}, 'rhs': {2.0: 0, 4: 1, 3.0: 2, False: 3}},
+            {'lhs': {False: 0, 2.0: 1, 4: 2, 1: 3}, 'rhs': {1.0: 0, 2.0: 1, 3.0: 2}},
+            {'lhs': {4.0: 0}, 'rhs': {}},
+            {'lhs': {0: 0, 4: 1, 1: 2, 3: 3, 2.0: 4}, 'rhs': {0.0, 2.0, 3.0}},
+            {'lhs': {}, 'rhs': {}},
+            {'lhs': {0: 0}, 'rhs': {3.0: 0}},
+        ]
+
+    def __iter__(self):
+        yield from iter(self.exemplars)
+
 
 def compare_keys(keys, keys_result):
+    """
+    Compare if the input and output keys have the same order and item identity
+    """
     keys_common = [k for k in keys if k in keys_result]
     is_identical = [k1 is kc for k1, kc in zip(keys, keys_common)]
     is_all_identical = all(is_identical)
@@ -126,14 +227,10 @@ def compare_keys(keys, keys_result):
     return status
 
 
-def run_test_case(testgen):
-
-    lhs = testgen.random_lhs()
-    rhs = testgen.random_rhs()
-
-    # Execute the candidate implementation
-    result = __and__(lhs, rhs)
-
+def analyze_result(lhs, rhs, result):
+    """
+    Get information about how the operation output relates to its inputs.
+    """
     # Analyze the result
     keys_lhs = list(lhs)
     keys_rhs = list(rhs)
@@ -148,28 +245,98 @@ def run_test_case(testgen):
         'lhs_keys': keys_lhs,
         'rhs_keys': keys_rhs,
         'result_keys': keys_result,
-        'type_LHS': type(lhs).__name__,
-        'type_RHS': type(rhs).__name__,
+        'LHS_type': type(lhs).__name__,
+        'RHS_type': type(rhs).__name__,
+        'rhs': rhs,
+        'lhs': lhs,
+        'result': result,
     }
     return row
 
 
+def _group_results(df):
+    # Used to generate static examples that cover the main cases.
+    import rich
+    import pandas as pd
+    # Breakup the rows into groups of cases with similar behavior
+    # Choose a subset of them as exemplars
+    grouper = ['RHS_order', 'LHS_order', 'RHS_id', 'LHS_id']
+
+    FIND_EXEMPLARS = 0
+    if FIND_EXEMPLARS:
+        chosen = []
+
+    for op, opgroup in df.groupby(['op']):
+        parts = []
+        print(f'--- {op} ---')
+        for _, group in opgroup.groupby(grouper):
+            to_show = group.drop(['rhs', 'lhs', 'result'], axis=1)
+            parts.append(to_show)
+            # rich.print(to_show.to_string())
+
+            if FIND_EXEMPLARS:
+                flags = (group['RHS_size'] > 2) & (group['LHS_size'] > 2)
+                chosen.extend(flags[flags].index[0:2])
+                flags = (group['RHS_size'] == 0) & (group['LHS_size'] == 0)
+                chosen.extend(flags[flags].index[0:1])
+                flags = (group['RHS_size'] == 0) & (group['LHS_size'] == 1)
+                chosen.extend(flags[flags].index[0:1])
+                flags = (group['RHS_size'] == 1) & (group['LHS_size'] == 0)
+                chosen.extend(flags[flags].index[0:1])
+                flags = (group['RHS_size'] == 1) & (group['LHS_size'] == 1)
+                chosen.extend(flags[flags].index[0:1])
+        rich.print(pd.concat(parts).to_string())
+
+    if FIND_EXEMPLARS:
+        import ubelt as ub
+        chosen = sorted(set(chosen))
+        chosen_df = df.loc[chosen]
+        # Here is a common case where having & would be really nice!
+        __and__ = CandidateOperations.__and_v1__
+        exemplars = [__and__(row, {'lhs', 'rhs'})
+                     for row in chosen_df.to_dict('records')]
+        print('exemplars = {}'.format(ub.urepr(exemplars, nl=1)))
+        list(chosen_df['lhs'].values)
+        list(chosen_df['rhs'].values)
+
+
 def main():
     # Initialize a seeded random number generator
-    rng = random.Random(3300082100142)
-    max_size = 5
-    testgen = TestCaseGenerator(max_size, rng)
 
-    niter = 100
+    RANDOMIZE = 0
+    if RANDOMIZE:
+        # Randonly generate cases
+        # rng = random.Random(3300082100142)
+        rng = random
+        max_size = 5
+        niter = 100
+        casegen = TestCaseGenerator(max_size, rng, niter)
+        casegen.next_case()
+    else:
+        # Use pre-defined cases
+        casegen = ExemplarCaseGenerator()
+
+    operations = [
+        CandidateOperations.__and_v1__,
+        CandidateOperations.__and_v2__,
+        CandidateOperations.__sub_v1__,
+        CandidateOperations.__sub_v2__,
+    ]
+
     rows = []
-    for _ in range(niter):
-        row = run_test_case(testgen)
-        rows.append(row)
+    for operation in operations:
+        for case in casegen:
+            rhs, lhs = case['rhs'], case['lhs']
+
+            # Execute the candidate implementation
+            result = operation(lhs, rhs)
+            row = analyze_result(lhs, rhs, result)
+            row['op'] = operation.__name__
+            rows.append(row)
 
     import pandas as pd
     df = pd.DataFrame(rows)
-    import rich
-    rich.print(df.to_string())
+    _group_results(df)
 
 
 if __name__ == '__main__':
